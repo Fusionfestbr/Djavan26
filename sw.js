d1c296300353a4b793bb2ccfcaea0b6931da7042
fuionapp <fusionfestbr@gmail.com>
2026-05-10 09:15:49 -0300

    feat: implement Progressive Web App support with service worker, web manifest, and icon assets

---
+++ sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'meus-ingressos-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './ingresso.html',
+  './home.css',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './assets/quentro.png',
+  './assets/djavan.png',
+  './assets/ticketmaster.png',
+  './assets/qr1.png',
+  './assets/qr2.png',
+  './assets/qr3.png',
+  './assets/qr4.png',
+  './assets/qr5.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(networkResponse => {
+          if (networkResponse && networkResponse.status === 200) {
+            const responseClone = networkResponse.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseClone);
+            });
+          }
+          return networkResponse;
+        });
+      })
+      .catch(() => caches.match('./index.html'))
+  );
+});
